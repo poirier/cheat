cad76d0eff9e640d6fbb846d4ad268e38361f209
Dan Poirier <dpoirier@caktusgroup.com>
2018-09-11 12:43:21 -0400

    Add page about gradually fixing a code base's style

--- conf.py
+++ conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Dan\'s Cheat Sheets'
-copyright = '2017, Dan Poirier'
+copyright = '2018, Dan Poirier'
 author = 'Dan Poirier'
 
 # The version info for the project you're documenting, acts as replacement for
